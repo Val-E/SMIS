be23741b12f8154633acea3d827f02549ccb0312
Val-E <30366552+Val-E@users.noreply.github.com>
2022-10-23 19:39:56 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,1 @@
-import logging 
-
-from settings import LOG_FILE, ENCODING
-
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    filename=LOG_FILE, 
-    encoding=ENCODING, 
-    level=logging.DEBUG
-)
-
+# mark as package
